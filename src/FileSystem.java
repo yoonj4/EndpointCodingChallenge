f8bdbcb249628078274524bd213d20d9427a9674
yoonj4 <justin.o.yoon@gmail.com>
2023-03-02 22:59:31 -0800

    Implement FileSystem constructor.

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -1,8 +1,25 @@
 package src;
 
-public class FileSystem {
-    public FileSystem() {
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+public class FileSystem {
+
+    private static class Directory {
+
+        private String name;
+        private SortedSet<Directory> subDirectories;
+
+        private Directory(String name) {
+            this.name = name;
+            subDirectories = new TreeSet<>();
+        }
+    }
+
+    private SortedSet<Directory> directories;
+
+    public FileSystem() {
+        directories = new TreeSet<>();
     }
 
     public void executeCommand(String command) {
